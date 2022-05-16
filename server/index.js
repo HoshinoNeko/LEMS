b526cee975b32d78b001147c1539bb454d116ff2
HoshinoNeko <akaruhoshino@outlook.com>
2022-05-16 18:04:56 +0800

    Structure adjust

--- server/index.js
+++ server/index.js
@@ -4,9 +4,15 @@ const fs = require('fs')
 const bodyParser = require('body-parser')
 const log = require('loglevel');
 const configPath = './src/config/config.json';
-const sql = require('model/db.js')
+const sql = require('./model/db.js')
 
 const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 let logLevel = config.system.log.level || 'error'
 log.setLevel(logLevel);
 log.debug(config);
+
+
+sql.query('SELECT * FROM `users`', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+});
--- server/src/model/db.js
+++ server/src/model/db.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const mysql = require('mysql');
-const configPath = './config/config.json';
+const configPath = '/server/config/config.json';
 const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 const connection = mysql.createPool(config.db)
 
