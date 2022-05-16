7402dfaf5d6706ca0e8fea1b26f37a261e69b326
Hoshino <21078047+HoshinoNeko@users.noreply.github.com>
2022-05-16 17:52:32 +0800

    init

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = new express()
+const fs = require('fs')
+const bodyParser = require('body-parser')
+const log = require('loglevel');
+const configPath = './src/config/config.json';
+const sql = require('model/db.js')
+
+const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+let logLevel = config.system.log.level || 'error'
+log.setLevel(logLevel);
+log.debug(config);
---
+++ server/model/db.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const mysql = require('mysql');
+const configPath = './config/config.json';
+const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+const connection = mysql.createPool(config.db)
+
+module.exports = connection
